0610d4fc9a2dc13165e3034eee785956bb4290f5
DBSunderland <davissunderland@creighton.edu>
2021-03-15 15:12:34 -0500

    15:12 update

--- forms/calculate/calculate.js
+++ forms/calculate/calculate.js
@@ -1,10 +1,11 @@
+/*
 function calcAvgSquare(num1, num2, num3) {
   let number1 = parseInt(num1)
   let number2 = parseInt(num2)
   let number3 = parseInt(num3)
   let multiplier = Math.pow(number1,2)
   let answer = ((number1 + number2 + number3)/3)*multiplier
-  console.log(answer)
+  console.log(`The answer is ${answer}.`)
   return answer
   }
 
@@ -13,26 +14,5 @@ let inpt2 = prompt("Enter the second of three numbers")
 let inpt3 = prompt("Enter the third of three numbers")
 
 calcAvgSquare(inpt1, inpt2, inpt3)
-
-
-/*
-Example function
-
-function testEven(num1, num2) {
-  let number1 = num1
-  let number2 = num2
-  if (number1 % 2 == 0) {
-    console.log(`${number1} is an even number!`)
-  } else {
-    console.log(`${number1} is not an even number.`)}
-  if (number2 % 2 == 0) {
-    console.log(`${number2} is an even number!`)
-  } else {
-    console.log(`${number2} is not an even number.`)}
-  }
-
-let inpt1 = prompt("Enter your first number")
-let inpt2 = prompt("Enter your second number")
-
-testEven(inpt1, inpt2)
 */
+
---
+++ forms/calculateEC/calculateEC.js
@@ -0,0 +1,39 @@
+/*
+let Remainder = ""
+let Average = ""
+
+function calcAvgSquare(num1, num2, num3) {
+  let number1 = parseInt(num1)
+  let number2 = parseInt(num2)
+  let number3 = parseInt(num3)
+  let multiplier = Math.pow(number1,2)
+  let answer = ((number1 + number2 + number3)/3)*multiplier
+  alert(`The answer is ${answer}.`)
+  return answer
+  }
+
+function divideTwo(num1, num2) {
+  let firstNumber = parseInt(num1)
+  let secondNumber = parseInt(num2)
+  let division = Math.floor(firstNumber / secondNumber)
+  let remainder = firstNumber % secondNumber
+  alert(`The result of dividing ${firstNumber} by ${secondNumber} is ${division} with a remainder of ${remainder}.`)
+  }
+
+whatchaGonnaDo = prompt("Do you want to do an average calculation or a remainder of the division of two numbers calculation? Enter Average or Remainder.")
+new_str = whatchaGonnaDo.toString()
+
+if (whatchaGonnaDo === "Remainder") {
+  let inptDivide1 = prompt("Enter the first of two numbers")
+  let inptDivide2 = prompt("Enter the second of two numbers")
+  divideTwo(inptDivide1, inptDivide2)
+} else if (whatchaGonnaDo === "Average") {
+  let inpt1 = prompt("Enter the first of three numbers")
+  let inpt2 = prompt("Enter the second of three numbers")
+  let inpt3 = prompt("Enter the third of three numbers")
+  calcAvgSquare(inpt1, inpt2, inpt3)
+} else {
+  alert("You didn't give a valid answer.")}
+
+*/
+
