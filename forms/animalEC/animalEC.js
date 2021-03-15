7fcb805211c0c7fb065afe238ed288b4f86b6855
DBSunderland <davissunderland@creighton.edu>
2021-03-15 14:31:01 -0500

    14:30 Update

--- forms/animalEC/animalEC.js
+++ forms/animalEC/animalEC.js
@@ -4,6 +4,7 @@ let newAnimal = prompt("Which animal would you like to add?")
 let lowerAnimal = newAnimal.toLowerCase()
 */
 
+/*
 for (i = 0; i <= 1; i++) {
   let myAnimals = ["dog", "cat", "horse", "meerkat"]
   let newAnimal = prompt("Which animal would you like to add?")
@@ -11,3 +12,4 @@ for (i = 0; i <= 1; i++) {
   myAnimals.push(lowerAnimal)
   console.log(`The last animal is a/an ${lowerAnimal}.`)
   }
+*/
---
+++ forms/bucketList/bucketList.js
@@ -0,0 +1,16 @@
+/*
+let doneWithBucketList = false
+let yesNo = ""
+let bucketList = []
+
+while (doneWithBucketList == false) {
+  yesNo = prompt("Do you want to add to your bucket list? Yes or No")
+  if (yesNo == "Yes") {
+    doneWithBucketList = false
+    let newItem = prompt("What would you like to add to your bucket list?")
+    bucketList.push(newItem)
+    console.log(newItem)
+ } else
+    doneWithBucketList = true
+  }
+*/
